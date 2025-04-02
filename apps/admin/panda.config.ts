bdba59e64680389a5a0d3e6469704b646a42900b
Socrates <sekpaliguidime@gmail.com>
2025-04-02 19:33:22 +0100

    code update

---
+++ apps/admin/panda.config.ts
@@ -0,0 +1,11 @@
+// import { defineConfig } from "@pandacss/dev";
+
+// export default defineConfig({
+//   //...
+//   include: [
+//     "./node_modules/@acme-org/design-system/dist/panda.buildinfo.json",
+//     "./src/**/*.{ts,tsx}",
+//   ],
+//   importMap: "@repo/pandacss",
+//   outdir: "styled-system",
+// });
